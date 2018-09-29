ed232da6d2dcb1a3843faffb7ce8171cd11a9acc
reechang <30851254+reechang@users.noreply.github.com>
2018-09-29 16:50:58 +0800

    Update c.sh

--- c.sh
+++ c.sh
@@ -249,7 +249,7 @@ apt-key add dotdeb.gpg
 #更新系统
 apt update -y
 #安装PHP 7和Sqlite 3
-apt -y install php7.0-cgi php7.0-fpm php7.0-curl php7.0-gd php7.0-mbstring php7.0-xml php7.0-sqlite3 sqlite3
+apt -y install php7.0-cgi php7.0-fpm php7.0-curl php7.0-gd php7.0-mbstring php7.0-xml php7.0-sqlite3 sqlite3 -y
 	judge "php+sqlite3 安装"
 rm -rf dotdeb.gpg
 }
